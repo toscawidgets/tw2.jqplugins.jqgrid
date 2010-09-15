079cd22b06952879d2a0fb02be680f415da5f978
Ralph Bean <ralph.bean@gmail.com>
2010-09-15 11:20:57 -0400

    reorganized file hierarchy

---
+++ tw2/jquery/jqgrid/defaults.py
@@ -0,0 +1,4 @@
+_jqgrid_version_    = '3.6.5'
+_jqgrid_name_       = 'jqGrid'
+_jqgrid_css_basename_   = 'ui.jqgrid'
+_jqgrid_locale_     = 'en'
--- tw2/jquery/jqgrid/widgets.py
+++ tw2/jquery/jqgrid/widgets.py
@@ -1,70 +1,18 @@
-from base import jQueryJSLink, jQueryCSSLink, jQueryPluginJSLink, jQueryPluginCSSLink, jQueryUIThemeCSSLink, jQueryUIJSLink
+from tw2.jquery.core.base import (
+    jQueryJSLink, jQueryCSSLink, jQueryPluginJSLink,
+    jQueryPluginCSSLink, jQueryUIThemeCSSLink, jQueryUIJSLink
+)
 from tw2.core.resources import encoder
 import formencode.validators as fv
 import defaults
 
 jquery_js = jQueryJSLink()
 
-####
-####
-#### JQuery plugins
-####
-####
-
-# SwitchView
-jswitchview_js = jQueryPluginJSLink(name=defaults._switchview_name_, version=defaults._switchview_version_)
-jswitchview_css = jQueryPluginCSSLink(name=defaults._switchview_name_, version=defaults._switchview_version_)
-jswitchview = jQueryJSLink(resources=[jswitchview_js, jswitchview_css])
-
-#Jcrop
-jcrop_css = jQueryPluginCSSLink(name=defaults._jcrop_name_, version=defaults._jcrop_version_)
-jcrop_js = jQueryPluginJSLink(name=defaults._jcrop_name_, version=defaults._jcrop_version_)
-
-# The usable piece, I couldn't figure out a better way to do dependencies
-jcrop = jQueryJSLink(resources = [jcrop_js, jcrop_css])
-
-#jquery.ui
-# Note we use the default smoothness theme
-jquery_ui_css = jQueryUIThemeCSSLink(name=defaults._ui_theme_name_, version=defaults._ui_version_)
-jquery_ui_js = jQueryUIJSLink(version=defaults._ui_version_)
-
-jquery_ui = jQueryJSLink(resources = [jquery_ui_css, jquery_ui_js])
-
-#jqgrid
 jqgrid_css = jQueryPluginCSSLink(name=defaults._jqgrid_name_, version = defaults._jqgrid_version_, basename = defaults._jqgrid_css_basename_)
 jqgrid_locale = jQueryPluginJSLink(name=defaults._jqgrid_name_, basename='grid.locale-%s' % defaults._jqgrid_locale_, subdir='js/i18n', version=defaults._jqgrid_version_)
 jqgrid_js = jQueryPluginJSLink(name=defaults._jqgrid_name_, version = defaults._jqgrid_version_, variant='min')
 
 jqgrid = jQueryJSLink(resources = [jquery_ui_css, jquery_ui_js, jqgrid_locale, jqgrid_js, jqgrid_css])
-
-####
-####
-#### Jquery enabled widgets
-####
-####
-
-jnullable_js = jQueryPluginJSLink(name=defaults._nullable_name_, version=defaults._nullable_version_)
-
-import tw2.forms.widgets
-import tw2.core as twc
-
-class NullableSelectionField(tw2.forms.widgets.SelectionField):
-    input_class = 'nullable'
-    title_text = twc.Param("Text to use as the tool tip for each radio button", default="Select an option, or click a selected option to deselect it.")
-    resources = [jquery_js, jnullable_js]
-
-    def prepare(self):
-        super(NullableSelectionField, self).prepare()
-        #Add the input_class value to the class list
-        for opt in self.options:
-            opt[0]['class'] = self.input_class
-            opt[0]['title'] = self.title_text
-
-class NullableRadioButtonList(NullableSelectionField, tw2.forms.widgets.RadioButtonList): pass
-
-class NullableRadioButtonTable(NullableSelectionField, tw2.forms.widgets.RadioButtonTable):
-    """TODO: Fix the prepare path"""
-    pass
 
 _pager_defaults = {'enableSearch': True, 'enableClear': True, 'gridModel': True}
 
