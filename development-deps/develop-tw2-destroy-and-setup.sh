2991bbb89590042d77615c3d833b6254a573c458
Ralph Bean <ralph.bean@gmail.com>
2010-09-15 13:06:38 -0400

    beef for dev scripts

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -16,15 +16,15 @@ pip install formencode
 pushd $devbase
 
 hg clone http://bitbucket.org/paj/tw2core || \
-        pushd tw2core && hg pull && popd
+        (pushd tw2core && hg pull && popd)
 hg clone http://bitbucket.org/paj/tw2devtools || \
-        pushd tw2devtools && hg pull && popd
+        (pushd tw2devtools && hg pull && popd)
 hg clone http://bitbucket.org/paj/tw2forms || \
-        pushd tw2forms && hg pull && popd
+        (pushd tw2forms && hg pull && popd)
 git clone git://github.com/ralphbean/tw2.jquery.core.git || \
-        pushd tw2.jquery.core && git pull && popd
+        (pushd tw2.jquery.core && git pull && popd)
 git clone git://github.com/ralphbean/tw2.jquery.ui.git || \
-        pushd tw2.jquery.ui && git pull && popd
+        (pushd tw2.jquery.ui && git pull && popd)
 
 pushd tw2core ;  python setup.py develop ; popd
 pushd tw2forms ; python setup.py develop ; popd
