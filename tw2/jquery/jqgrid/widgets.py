c9818c23d0aa49c19022ee69c10081517f38ba32
Ralph Bean <ralph.bean@gmail.com>
2010-09-16 13:53:41 -0400

    moving ahead with jqGrid demo.

--- tw2/jquery/jqgrid/widgets.py
+++ tw2/jquery/jqgrid/widgets.py
@@ -4,6 +4,8 @@
 from tw2.core.resources import encoder
 import tw2.core as twc
 
+import tw2.jquery.core
+
 import formencode.validators as fv
 import base
 
@@ -24,24 +26,18 @@ class jqGridFilterSchema(fe.Schema):
     searchOper = fv.String(if_empty=None, not_empty=False, if_missing=None)
 
 
-class jqGrid(twc.Widget):
+class jqGridWidget(tw2.jquery.core.JQueryWidget):
     resources = [base.jqgrid]
-    url = twc.Param("Url that jqGrid should pull its data from", default=None)
-    options = twc.Param("Extra options to pass to jqgrid", default={})
-    pager_options = twc.Param(
-        "Extra options to pass to jqgrid's pager", default=_pager_defaults)
-    paginate = twc.Param("Will the widget show the pager?", default=None)
     template = "tw2.jquery.jqgrid.templates.jqgrid"
+    
+    options = twc.Param("Configuration options to pass to jqgrid", default={})
 
     def prepare(self):
-        super(jqGrid, self).prepare()
-        self._url = encoder.encode(self.url)
+        super(jqGridWidget, self).prepare()
+        if not self.options:
+            raise ValueError, 'jqGridWidget must be supplied a dict of options'
+
+        if not 'url' in self.options:
+            raise ValueError, "jqGridWidget must be supplied a url in options"
+
         self._options = encoder.encode(self.options)
-        self._pager_options = {}
-        for k, v in self.pager_options.iteritems():
-            self._pager_options[k] = encoder.encode(v)
-        if self.paginate:
-            self._pager = 'pag_' + self.attrs['id']
-        else:
-            self._pager = None
-        self._pager = encoder.encode(self._pager)
