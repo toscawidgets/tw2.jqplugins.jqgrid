db78728fb8870bafc5cdce6e230611953bbcd1dd
Ralph Bean <ralph.bean@gmail.com>
2010-09-18 14:14:09 -0400

    manifest for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tw2.jquery.jqgrid',
-    version='2.0a1',
+    version='2.0a2',
     description='',
     author='Ralph Bean',
     author_email='ralph.bean@gmail.com',
