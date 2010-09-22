700c3aaa5040f6e67ec8e4d8f463eb5bf8a8dc1c
Ralph Bean <ralph.bean@gmail.com>
2010-09-22 13:11:58 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tw2.jquery.jqgrid',
-    version='2.0a11',
+    version='2.0a12',
     description='',
     author='Ralph Bean',
     author_email='ralph.bean@gmail.com',
