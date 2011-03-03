40fb2591113c95511a3e09d5ab9bf54536110167
Ralph Bean <ralph.bean@gmail.com>
2011-03-03 13:40:34 -0500

    namespace refactor (missed some)

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -24,8 +24,8 @@ hg clone http://bitbucket.org/paj/tw2forms || \
         (pushd tw2forms && hg pull && popd)
 hg clone http://bitbucket.org/toscawidgets/tw2jquery || \
         (pushd tw2jquery && hg pull && popd)
-git clone http://github.com/ralphbean/tw2.jquery.plugins.ui.git || \
-        (pushd tw2.jquery.plugins.ui && git pull && popd)
+git clone http://github.com/ralphbean/tw2.jqplugins.ui.git || \
+        (pushd tw2.jqplugins.ui && git pull && popd)
 #hg clone https://ralphbean@bitbucket.org/toscawidgets/tw2jquery || \
 #        (pushd tw2jquery && hg pull && popd)
 
@@ -33,6 +33,6 @@ pushd tw2core ;  python setup.py install ; popd
 pushd tw2forms ; python setup.py install ; popd
 pushd tw2devtools ; python setup.py install ; popd
 pushd tw2jquery ; python setup.py install_lib install_egg_info ; popd
-pushd tw2.jquery.plugins.ui ; python setup.py install_lib install_egg_info ; popd
+pushd tw2.jqplugins.ui ; python setup.py install_lib install_egg_info ; popd
 
 popd # $devbase
