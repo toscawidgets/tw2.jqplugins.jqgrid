51b8aeea4fb3517a4b83842be4a04e1c6d33526c
Ralph Bean <rbean@redhat.com>
2012-03-01 12:49:23 -0500

    Removed filter schema.  No-one is using it (I hope).

--- tw2/jqplugins/jqgrid/widgets/core.py
+++ tw2/jqplugins/jqgrid/widgets/core.py
@@ -8,26 +8,9 @@ import tw2.jquery
 import tw2.jquery.base as tw2_jq_c_b
 import tw2.jqplugins.ui.base as tw2_jq_ui
 
-import formencode.validators as fv
 from tw2.jqplugins.jqgrid import base
 
 _pager_defaults = {'enableSearch': True, 'enableClear': True, 'gridModel': True}
-
-import formencode as fe
-import formencode.validators as fv
-# TODO -- how to make use of this?  a tw2 controller?
-class jqGridFilterSchema(fe.Schema):
-    allow_extra_fields=True
-
-    page = fv.Int(if_empty=1, not_empty=False, if_missing=1)
-    rows = fv.Int(if_empty=10, not_empty=False, if_missing=1)
-    sidx = fv.String(if_empty=None, not_empty=False, if_missing=None)
-    sord = fv.String(if_empty=None, not_empty=False, if_missing=None)
-    _search = fv.StringBool(if_empty=False, not_empty=False, if_missing=False)
-    searchField = fv.String(if_empty=None, not_empty=False, if_missing=None)
-    searchString = fv.String(if_empty=None, not_empty=False, if_missing=None)
-    searchOper = fv.String(if_empty=None, not_empty=False, if_missing=None)
-
 
 class jqGridWidget(tw2_jq_ui.JQueryUIWidget):
     resources = [
